391432bb63f95d928212fb6000a11496d5eb678e
Marc Jarvis <jarvis.marc@gmail.com>
2015-02-13 01:54:22 -0500

    Added animation to the fancier remote depot.
    	new file:   Assets/Models/Objects.meta
    	new file:   Assets/Models/Objects/depotGrow.controller
    	new file:   Assets/Models/Objects/depotGrow.controller.meta
    	new file:   Assets/Models/Objects/remoteDepot.fbx
    	new file:   Assets/Models/Objects/remoteDepot.fbx.meta
    	modified:   Assets/Prefabs/Characters/Blitz.prefab
    	modified:   Assets/Prefabs/Characters/Syphen.prefab
    	modified:   Assets/Prefabs/Characters/ThirdPersonCharacter.prefab
    	modified:   Assets/Prefabs/DestructableObjects/InukshukGrp.prefab
    	new file:   Assets/Prefabs/Fractures/squareTiles.prefab
    	new file:   Assets/Prefabs/Fractures/squareTiles.prefab.meta
    	new file:   Assets/Prefabs/SupplyDepot/remoteDepot.prefab
    	new file:   Assets/Prefabs/SupplyDepot/remoteDepot.prefab.meta
    	modified:   Assets/Prefabs/Weaponry/bombPull.prefab
    	modified:   Assets/Prefabs/Weaponry/projectilePush.prefab
    	new file:   Assets/Scenes/tests/depotTest1.unity
    	new file:   Assets/Scenes/tests/depotTest1.unity.meta
    	modified:   Assets/Scenes/tests/fractureTest2.unity
    	new file:   Assets/Scripts/supplyDepot/depotAbsorb.cs
    	new file:   Assets/Scripts/supplyDepot/depotAbsorb.cs.meta

---
+++ Assets/Scripts/supplyDepot/depotAbsorb.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class depotAbsorb : MonoBehaviour {
+	public string supplyItemTag;
+
+	public int capacity = 5000;
+	public int resourceVal = 100;
+	public int currentStock = 0;
+
+	private Animator myAnim;
+
+	void Start(){
+		myAnim = this.GetComponent<Animator>();
+		myAnim.SetBool("isStopped", true);
+		myAnim.SetBool("isPlaying", false);
+		myAnim.speed = 0.0f;
+	}
+
+
+	void OnTriggerEnter(Collider other){
+
+		/*
+		 * replace 10 with other.gameObject.value
+		 * 
+		 *
+		 */
+		if(other.tag == supplyItemTag){
+			currentStock = currentStock + resourceVal;
+
+			if ((this.currentStock + resourceVal) < capacity) {
+				Debug.Log("Growing");
+				this.currentStock += resourceVal;
+				myAnim.SetBool("isPlaying", true);
+				myAnim.SetBool("isStopped", false);
+				myAnim.Play("depotGrow", 0, myAnim.GetCurrentAnimatorStateInfo(0).length * currentStock/capacity);
+			}else{
+				Debug.Log ("Resource Depot FuLL");
+			}
+			//this.transform.parent.transform.localScale += (Vector3.up * 0.1f);
+			Destroy( other.gameObject );
+		}
+
+	}
+
+}
