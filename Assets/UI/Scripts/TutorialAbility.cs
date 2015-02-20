96fc18e713cb8df5bf79abf3cc6cdfb4c9328475
Monica Ung <monicaung3@gmail.com>
2015-02-20 03:32:23 -0500

    Forgot to commit all the new files
    because I need sleep

---
+++ Assets/UI/Scripts/TutorialAbility.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialAbility : MonoBehaviour {
+
+	public string inputName = "P1_Fire1";
+	public GameObject gameManager;
+	private TutorialManager tutManager;
+
+	void Start () {
+		tutManager = gameManager.GetComponent<TutorialManager> ();
+	}
+	
+	void Update () {
+		if (Input.GetButtonDown (inputName)) {
+			tutManager.NextTutorial();
+		}
+	}
+}
