ed9648396597c85a596b616fa556b23eaa0db74d
Monica Ung <monicaung3@gmail.com>
2015-02-25 00:55:42 -0500

    Added icons to tutorials, changed window colours. Changed resource colour

--- Assets/UI/Scripts/TutorialPanel.cs
+++ Assets/UI/Scripts/TutorialPanel.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class TutorialPanel : MonoBehaviour {
 
-	public float width = 0.4f;
-	public float height = 0.4f;
 	public float left = 0.0f;
 	public float top = 0.0f;
 
@@ -15,13 +13,12 @@ public class TutorialPanel : MonoBehaviour {
 		//Get and set panel size and position
 		rect = gameObject.transform.GetChild (0).GetComponent<RectTransform> ();
 		rect.anchoredPosition = new Vector2 (Screen.width * left, Screen.height * top);
-		rect.sizeDelta = new Vector2 (Screen.width * width, Screen.height * height);
+//		rect.sizeDelta = new Vector2 (width, height);
 
 		//Get and set arrow position
 		arrow = gameObject.transform.GetChild (1).GetComponent<RectTransform>();
 		arrow.sizeDelta = new Vector2(Screen.width*0.05f, Screen.width*0.05f);
 		arrow.anchoredPosition = new Vector2 (Screen.width * left, (Screen.height * top) + arrow.sizeDelta.y);
-
 
 		Debug.Log ("box anchor:" + rect.anchoredPosition);
 		Debug.Log ("box size:" + rect.sizeDelta);
