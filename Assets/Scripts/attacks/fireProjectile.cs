41c60efd193de2dbbadf88dccf56eae67553a546
Marc Jarvis <jarvis.marc@gmail.com>
2015-02-25 23:14:08 -0500

    Tweaked projectile parameters.
    	new file:   Assets/Prefabs/Characters/Blitz.prefab
    	new file:   Assets/Prefabs/Characters/Blitz.prefab.meta
    	new file:   Assets/Prefabs/Characters/Syphen.prefab
    	new file:   Assets/Prefabs/Characters/Syphen.prefab.meta
    	modified:   Assets/Scripts/attacks/fireProjectile.cs

--- Assets/Scripts/attacks/fireProjectile.cs
+++ Assets/Scripts/attacks/fireProjectile.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using System.Collections;
 using GamepadInput;
 
-public enum ProjectileAction { LAUNCH, BEAM, ATTACH_TO_SELF, REMOTE_CTRL }
+public enum ProjectileAction { THROW, BEAM, ATTACH_TO_SELF, REMOTE_CTRL }
 public enum ProjectileTriggerButton { LEFT, RIGHT }
 
 /**
@@ -19,27 +19,22 @@ public class fireProjectile: MonoBehaviour {
 	public float _drag = 5;
 	public bool _makeChild = false;
 
-	public bool _isControllable = false;
-	
 	Vector3 forward;
 	public float _cooldown = 3;
 	float _cooldownTimer;
 	float _triggerThreshold = 0.20f;
 
+	public bool _isControllable = false;
 	bool _leftTriggerHeld;
 	bool _rightTriggerHeld;
 	bool _alreadyFired = false;
 	GameObject _controllable;
-
-	//public string inputName = "Fire1";
-	public ProjectileAction _projectileAction = ProjectileAction.LAUNCH;
+	
+	public ProjectileAction _projectileAction = ProjectileAction.THROW;
 	public ProjectileTriggerButton _projectileButton = ProjectileTriggerButton.LEFT;
-	//public bool isBomb = false;
+
 	GamePad.Index _padIndex = GamePad.Index.One;
-	
-	//	public enum TrajectoryType {Straight, Lob, Drop, Attach};
-	//	public TrajectoryType style = TrajectoryType.Straight;
-	
+
 	// Use this for initialization
 	void Start () {
 		_cooldownTimer = _cooldown;
@@ -48,33 +43,38 @@ public class fireProjectile: MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		_cooldownTimer -= Time.deltaTime;
-		/*if ( Input.GetButtonDown( inputName ) ){
-			Fire ();
-			cooldown = 0;
-		}*/
+
 		_leftTriggerHeld = (GamePad.GetTrigger (GamePad.Trigger.LeftTrigger, _padIndex) > _triggerThreshold);
 		_rightTriggerHeld = (GamePad.GetTrigger (GamePad.Trigger.RightTrigger, _padIndex) > _triggerThreshold);
-		if (_cooldownTimer <= 0.0f) {
+		if (_cooldownTimer <= 0.0f) 
+		{
 			if ( (_leftTriggerHeld && _projectileButton == ProjectileTriggerButton.RIGHT) 
-			|| (_rightTriggerHeld && _projectileButton == ProjectileTriggerButton.LEFT) ){
-				if (_projectileAction == ProjectileAction.LAUNCH) {
-					networkView.RPC("LaunchProjectile", RPCMode.All, _offset, _magnitude, _makeChild);
+			|| (_rightTriggerHeld && _projectileButton == ProjectileTriggerButton.LEFT) )
+			{
+				if (_projectileAction == ProjectileAction.THROW) {
+					if(Network.isClient || Network.isServer){
+						networkView.RPC("LaunchProjectile", RPCMode.All, _offset, _magnitude, _makeChild);
+					}else{
+						LaunchProjectile(_offset, _magnitude, _makeChild);
+					}
 				} else if (_projectileAction == ProjectileAction.BEAM) {
-					BeamAttack ();
+					if(Network.isClient || Network.isServer){
+						networkView.RPC("BeamAttack", RPCMode.All);
+					}else{
+						BeamAttack ();
+					}
 				} else if (_projectileAction == ProjectileAction.REMOTE_CTRL) {
-					if( _isControllable ){
-						if(!_alreadyFired){
-							LaunchControllable();
-							_alreadyFired = true;
-							// freeze the player, make this more efficientefficient? (i.e. don't use  findgameobject method)
-							GameObject.FindGameObjectWithTag("Player").GetComponent<DeftPlayerController>().enabled = false;
-						}
-						// TODO: control the ball
+					if(!_alreadyFired){
+						LaunchControllable();
+						_alreadyFired = true;
+						// freeze the player, make this more efficientefficient? (i.e. don't use  findgameobject method)
+						GameObject.FindGameObjectWithTag("Player").GetComponent<DeftPlayerController>().enabled = false;
+					}
+					// TODO: control the ball
 				}
-				_cooldownTimer = _cooldown;
+			_cooldownTimer = _cooldown;
 			}
 		}
-
 		// exited the remote control mode
 		if (!_leftTriggerHeld && _alreadyFired) {
 			_alreadyFired = false;
@@ -83,7 +83,6 @@ public class fireProjectile: MonoBehaviour {
 			// unfreeze player, make this more efficient? (i.e. don't use  findgameobject method)
 			GameObject.FindGameObjectWithTag("Player").GetComponent<DeftPlayerController>().enabled = true;
 		}
-	}
 	}
 
 	[RPC]
@@ -103,9 +102,9 @@ public class fireProjectile: MonoBehaviour {
 
 	[RPC]
 	void BeamAttack(){
-				GameObject clone;
-				clone = Instantiate (_projectile, transform.position + _offset, transform.rotation) as GameObject;
-				//clone.rigidbody.velocity = transform.TransformDirection( trajectory * magnitude );
+		GameObject clone;
+		clone = Instantiate (_projectile, transform.position + _offset, transform.rotation) as GameObject;
+		//clone.rigidbody.velocity = transform.TransformDirection( trajectory * magnitude );
 
 		forward = Camera.main.transform.TransformDirection(Vector3.forward);
 		forward = forward.normalized;
@@ -120,11 +119,9 @@ public class fireProjectile: MonoBehaviour {
 		GameObject clone;
 		clone = Instantiate( _projectile, transform.position + _offset, transform.rotation ) as GameObject;
 		//clone.rigidbody.velocity = transform.TransformDirection( trajectory * magnitude );
-		
-		if (_isControllable) {
-			_controllable = clone;
-			_controllable.GetComponent<TimedLifespan> ().enabled = false;
-		}
+
+		_controllable = clone;
+		_controllable.GetComponent<TimedLifespan> ().enabled = false;
 		
 		forward = Camera.main.transform.TransformDirection(Vector3.forward);
 		forward = forward.normalized;
